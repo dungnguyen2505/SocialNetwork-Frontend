6823358bbf631d67cd34b7474bee5ae996c4e920
viettt76 <viettt76@gmail.com>
2024-10-22 12:07:29 +0700

    fix chat popup

--- src/components/ChatPopup/ChatPopup.js
+++ src/components/ChatPopup/ChatPopup.js
@@ -15,49 +15,49 @@ import useClickOutside from '~/hook/useClickOutside';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 
 const initialListMessage = [
-        {
-            id: '67a43e79-1318-44ad-8303-f7a195795fae',
+    {
+        id: '67a43e79-1318-44ad-8303-f7a195795fae',
         sender: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
         receiver: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
-            message: 'ê',
-        },
-        {
-            id: 'f1ae90b2-9409-4fdc-afa0-b37c4726af03',
+        message: 'ê',
+    },
+    {
+        id: 'f1ae90b2-9409-4fdc-afa0-b37c4726af03',
         sender: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
         receiver: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
-            message: 'hello anh em nha mình đang bạn xíu tí mình vô nhé pp ae',
-        },
-        {
-            id: '41663581-c79c-4014-8514-5308942a5280',
+        message: 'hello anh em nha mình đang bạn xíu tí mình vô nhé pp ae',
+    },
+    {
+        id: '41663581-c79c-4014-8514-5308942a5280',
         sender: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
         receiver: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
-            message: 'fasdfashdfasdfjasdfhasdfuyeiwthbdsghdsfkgdfg',
-        },
-        {
-            id: '0d50ed85-3d16-4a98-8779-7cbbaec7c24b',
+        message: 'fasdfashdfasdfjasdfhasdfuyeiwthbdsghdsfkgdfg',
+    },
+    {
+        id: '0d50ed85-3d16-4a98-8779-7cbbaec7c24b',
         sender: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
         receiver: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
-            message: 'tyutyu',
-        },
-        {
-            id: '0b3785fc-ee00-44cc-8041-884b50b02b32',
+        message: 'tyutyu',
+    },
+    {
+        id: '0b3785fc-ee00-44cc-8041-884b50b02b32',
         sender: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
         receiver: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
-            message: 'ghghj',
-        },
-        {
-            id: '1c4a40fb-ebe7-42a6-8b77-32b44f75e3db',
+        message: 'ghghj',
+    },
+    {
+        id: '1c4a40fb-ebe7-42a6-8b77-32b44f75e3db',
         sender: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
         receiver: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
-            message: 'gjhj',
-        },
-        {
-            id: '07f59b7f-6bf6-4dc2-9f29-17611c4527fc',
+        message: 'gjhj',
+    },
+    {
+        id: '07f59b7f-6bf6-4dc2-9f29-17611c4527fc',
         sender: '80fe9c6d-3b7b-44e0-a8bf-226d4c52384e',
         receiver: '72348b73-1997-41bc-a7f3-bccba9cf99d0',
-            message: 'tyutyu',
-        },
-    ];
+        message: 'tyutyu',
+    },
+];
 const ChatPopup = ({ friend }) => {
     const { ref: chatPopupRef, isComponentVisible: isFocus, setIsComponentVisible: setIsFocus } = useClickOutside(true);
 
@@ -113,7 +113,6 @@ const ChatPopup = ({ friend }) => {
                 console.error('Error establishing connection:', error);
             }
         };
-        fetchMessages();
 
         startConnection();
 
@@ -134,13 +133,13 @@ const ChatPopup = ({ friend }) => {
 
             setMessages((prev) => {
                 return [
-                ...prev,
-                {
-                    id: null,
-                    sender: userInfo?.id,
+                    ...prev,
+                    {
+                        id: null,
+                        sender: userInfo?.id,
                         receiver: friend?.id,
                         message,
-                },
+                    },
                 ];
             });
 
